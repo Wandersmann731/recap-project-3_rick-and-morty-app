f4ab57081d5823dbe49e25d1309289f876cf3c63
Ana Pecic <ana.pecic97@gmail.com>
2024-07-17 15:50:09 +0200

    anleitung pagination

---
+++ components/NavPagination/NavPagination.js
@@ -0,0 +1,11 @@
+//next steps
+//1/function call von fetchCharacters mit der richtigen page:
+// fetchCharacters(`${apiEndpointUrl}?page=${page}`);
+
+//2/ eventlistener auf weiter und zurück
+// mit folgenden anforderungen:
+// - verhindern dass die seitenanzahl maxPage übersteigt
+// - page index ändert sich
+// - fetch characters wird richtig gecallt
+
+//3/ aktualisieren des pagination displays
