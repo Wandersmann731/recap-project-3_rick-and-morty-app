b8e57f719ccb784d4256865226c8039b7b04d814
Hauke Lauth <haukelauthgooglemail.com>
2024-07-17 10:52:55 +0200

    add card informations

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,2 +1,29 @@
-export function CharacterCard() {}
-const list = document.createElement("list");
+export function CharacterCard(
+  characterImage,
+  characterName,
+  characterStatus,
+  characterType,
+  characterEpisodeLength
+) {
+  const li = document.createElement("li");
+  li.classList.add("card");
+
+  li.innerHTML = `
+<div class="card__image-container">
+<img class="card__image" src="${characterImage}" alt"${characterName}">
+<div class="card__image-gradient"></div>
+</div>
+<div class="card__content">
+<h2 class=" card__title">${characterName}</h2>
+    <dl class="card__info">
+        <dt class="card__info-title">Status</dt>
+        <dd class="card__info-description">${characterStatus}</dd>
+        <dt class="card__info-title">Type</dt>
+        <dd class="card__info-description">${characterType}</dd>
+        <dt class="card__info-title">Occurrences</dt>
+        <dd class="card__info-description">${characterEpisodeLength}</dd>
+    </dl>
+    </div>
+`;
+  //return li;
+}
